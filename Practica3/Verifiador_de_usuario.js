81e34e050a6639cde373df06521ff57e7035d66f
Leonardo Garcia Luna <124049701@upq.edu.mx>
2025-09-26 20:25:22 -0600

    verificador de usuario

---
+++ Practica3/Verifiador_de_usuario.js
@@ -0,0 +1,13 @@
+function verificarUsuario(usuario) {
+  return new Promise((resolve, reject) => {
+    if (usuario === "admin") {
+      resolve("Acceso concedido");
+    } else {
+      reject("Acceso denegado");
+    }
+  });
+}
+
+verificarUsuario("admin")
+  .then((mensaje) => console.log(mensaje))
+  .catch((error) => console.log(error));
