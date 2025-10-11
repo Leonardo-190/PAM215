eefeb52d0e52849e6e6bf779349cd445f2f3c160
Leonardo Garcia Luna <124049701@upq.edu.mx>
2025-10-10 20:16:35 -0600

    Se crearon los apartados, los botones y las funciones

--- intro/App.js
+++ intro/App.js
@@ -1,19 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+//1.import:zona de importaciones 
 
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import React, { useState } from 'react';
+//2.Main Zona de componentes
 export default function App() {
+  const[Contador, SetContador]= useState(0);
   return (
+
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Contador:{Contador}</Text>
+      <Button title='incrementar' onPress={()=>SetContador(Contador+1)}> </Button>
+      <Button title='decrementar' onPress={()=>SetContador(Contador-1)}> </Button>
+      <Button title='reiniciar' onPress={()=>SetContador(Contador-Contador)}> </Button>
       <StatusBar style="auto" />
     </View>
+
   );
 }
-
+//3. Estilos y posicionamiento
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#c8ff00ff',
     alignItems: 'center',
     justifyContent: 'center',
   },
