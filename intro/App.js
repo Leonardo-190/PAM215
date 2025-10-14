4a13d6eb54a9eb00a52432b82b92f09d1c348d40
Leonardo Garcia Luna <124049701@upq.edu.mx>
2025-10-13 20:30:00 -0600

    Pantalla Principal

--- intro/App.js
+++ intro/App.js
@@ -1,29 +1,13 @@
 //1.import:zona de importaciones 
+import ContadorScreen from './Screens/ContadorScreen';
 
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button } from 'react-native';
-import React, { useState } from 'react';
 //2.Main Zona de componentes
+
 export default function App() {
-  const[Contador, SetContador]= useState(0);
+  
+
   return (
-
-    <View style={styles.container}>
-      <Text>Contador:{Contador}</Text>
-      <Button title='incrementar' onPress={()=>SetContador(Contador+1)}> </Button>
-      <Button title='decrementar' onPress={()=>SetContador(Contador-1)}> </Button>
-      <Button title='reiniciar' onPress={()=>SetContador(Contador-Contador)}> </Button>
-      <StatusBar style="auto" />
-    </View>
-
+<ContadorScreen></ContadorScreen>
   );
 }
-//3. Estilos y posicionamiento
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#c8ff00ff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
